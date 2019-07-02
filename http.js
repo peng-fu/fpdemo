085d1ce5aedeb523cd9c5d0a658aa54f481c2462
Peng Fu <peng.fu@gaiafintech.com>
2019-07-02 17:38:53 +0800

    asd

---
+++ http.js
@@ -0,0 +1,39 @@
+﻿var http = require('http');
+var url = require("url");
+var body = require("body-parser");
+var express = require('express');
+var data = require('./jsondata');
+var app = express();
+
+//配置post请求参数获取
+app.use(body.urlencoded({ extended: false })); 
+
+//设置跨域访问
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1');
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
+//设置返回数据格式
+var result = {
+    "status": "200",
+    "message": "success",
+    "data": []
+}
+
+app.get('/user', function(req, res) {
+    console.log("收到get请求")
+	console.log(data.udata);
+	res.send(JSON.stringify(data.udata));
+})
+
+app.post("/",(req,res) => {
+	//获取post传参
+	let name = req.body.name
+})
+
+app.listen(8080, 'localhost');
---
+++ jsondata.js
@@ -0,0 +1,28 @@
+let jdata = [
+	{
+		id:1,
+		name:"joel",
+		age:18,
+		height:172,
+	},
+	{
+		id:2,
+		name:"wk",
+		age:21,
+		height:182,
+	},
+	{
+		id:3,
+		name:"nuerd",
+		age:22,
+		height:175,
+	},
+	{
+		id:4,
+		name:"dlb",
+		age:19,
+		height:165,
+	}
+]
+
+exports.udata = jdata
