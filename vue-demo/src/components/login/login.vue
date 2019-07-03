b3951b3199b95342f09fa0d3af8a72b0205e23a3
Peng Fu <peng.fu@gaiafintech.com>
2019-07-03 15:32:33 +0800

    aa

---
+++ vue-demo/src/components/login/login.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="main">
+        <transition name="fade">
+            <div class="loginDiv " v-show="show">
+                <div class="un " style="animated-detay">
+                    <p class="fl tx"> 用户名:</p>
+                    <i-input placeholder="请输入..." style="width: 300px"></i-input>
+                </div>
+                <div class="pw ">
+                   <p class="fl tx"> 密码:</p>
+                    <input type="password" />
+                </div>
+                <div>
+                    <router-link class="fr" to="/regist">注册</router-link>
+                    <router-link class="fl" to="/forget">忘记密码</router-link>
+                </div>
+                <Button class="info">登录</Button>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            show:false,
+        }
+    },
+
+    mounted(){
+        this.show = true;
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<style>
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+
+.main{
+    background-color: aquamarine;
+    width: 100%;
+    height: 100%;
+    padding-top: 25vh;
+}
+.loginDiv{
+    width: 600px;
+    height: 500px;
+    background-color: white;
+    border-radius:5px;
+    box-shadow: 1px 2px 2px slategrey;
+    border: none; 
+    margin: 0 auto;
+    padding:30px 80px; 
+    font-size: 24px
+}
+.un{
+    margin: 20px 0;
+}
+.pw{
+   margin: 20px 0; 
+}
+.un input, .pw input{
+    border: none;
+    border-bottom: 1px solid #cccccc;
+}
+.tx{
+    width: 200px;
+    text-align: right
+}
+
+</style>
---
+++ vue-demo/src/router.js
@@ -0,0 +1,58 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Login from "./components/login/login";
+
+import store from "./store"
+
+Vue.use(Router);
+
+const router = new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "login",
+      component: Login
+    },
+
+    
+
+    {
+      path: "/login",
+      name: "login",
+      component: Login
+    },
+
+    {
+      path: "/about",
+      name: "about",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
+    }
+  ]
+});
+
+router.beforeEach((to, from, next) => {
+  console.log(to.meta.requireAuth);
+  if (to.meta.requireAuth) {  // 判断该路由是否需要登录权限
+    console.log(store.state.token);
+      if (store.state.token) {  // 通过vuex state获取当前的token是否存在
+          next();
+      }
+      else {
+          next({
+              path: '/login',
+              query: {redirect: to.fullPath}  // 将跳转的路由path作为参数，登录成功后跳转到该路由
+          })
+      }
+  }
+  else {
+      next();
+  }
+})
+
+export default router;
